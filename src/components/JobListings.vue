380e50b19457260dac017c797547adb0f77b9c26
Ian Jay's M2 Mac Mini <ianjaypadios@Ian-Jays-Mac-mini.local>
2025-10-19 05:20:22 +0800

    Fetch data from json file

--- src/components/JobListings.vue
+++ src/components/JobListings.vue
@@ -1,6 +1,14 @@
 <script setup>
+import jobData from '@/jobs.json';
+import {ref} from 'vue';
+
+const jobs = ref(jobData);
+console.log(jobs.value);
+
 </script>
 
 <template>
-    Jobs Listings
+    <div v-for="job in jobs" :key="job.id">
+        {{ job.title }}
+    </div>
 </template>
